298f63b1804cfe959f4b2832384c4dfe5df5fa45
devakumar <devakumar@devakumar>
2022-10-08 13:49:22 +0530

    oops constructor

---
+++ src/Oopsconcepts/Consuctor.java
@@ -0,0 +1,15 @@
+package Oopsconcepts;
+
+public class Consuctor {
+	Consuctor(){
+		System.out.println("hellottftg");
+	}
+
+	public static void main(String[] args) {
+		
+		Consuctor1 s=new Consuctor1();
+		     s.	m1();
+		     
+	}
+
+}
